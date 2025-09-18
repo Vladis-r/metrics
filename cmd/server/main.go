4ae3fa3a63be4708fcddf0b9eed85ab8deede3d9
Vladislav <ragozin.vladis@gmail.com>
2025-09-18 18:58:01 +0300

    iter3: up.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 
 	"github.com/Vladis-r/metrics.git/internal/handler"
-	models "github.com/Vladis-r/metrics.git/internal/model"
 )
 
 func main() {
 	r := gin.Default()                 // Create a new Gin instance
 	r.LoadHTMLGlob("templates/*.html") // Load HTML templates
 	r.Static("/static", "./static")    // Serve static files from the "static" directory
-
-	// Storage := models.NewMemStorage()
-	fmt.Println("Storage: ", models.Storage)
 
 	// handlers
 	r.GET("/", handler.Root)
