858f63c950dcf0389a14a31aef46a67422f9a4c9
Vladislav <ragozin.vladis@gmail.com>
2025-10-15 19:45:40 +0300

    iter5: fixed parse envs.

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -20,20 +21,21 @@ func GetConfig() *Config {
 
 	flag.Parse()
 
-	switch {
-	case os.Getenv("ADDR") != "":
-		c.Addr = os.Getenv("ADDR")
-	case os.Getenv("REPORT_INTERVAL") != "":
-		if i, err := strconv.Atoi(os.Getenv("REPORT_INTERVAL")); err == nil {
-			if i > 0 {
-				c.ReportInterval = i
-			}
+	if addr := os.Getenv("ADDR"); addr != "" {
+		c.Addr = addr
+	}
+	if reportStr := os.Getenv("REPORT_INTERVAL"); reportStr != "" {
+		if i, err := strconv.Atoi(reportStr); err == nil && i > 0 {
+			c.ReportInterval = i
+		} else {
+			fmt.Printf("Invalid REPORT_INTERVAL, must be positive: %s\n", reportStr)
 		}
-	case os.Getenv("POLL_INTERVAL") != "":
-		if i, err := strconv.Atoi(os.Getenv("POLL_INTERVAL")); err == nil {
-			if i > 0 {
-				c.PollInterval = i
-			}
+	}
+	if pollStr := os.Getenv("POLL_INTERVAL"); pollStr != "" {
+		if i, err := strconv.Atoi(pollStr); err == nil && i > 0 {
+			c.PollInterval = i
+		} else {
+			fmt.Printf("Invalid POLL_INTERVAL, must be positive: %s\n", pollStr)
 		}
 	}
 	return c
