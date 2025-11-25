c637341c7ca1e9786fca4eaf037835b04bd8d6f4
Vladislav <ragozin.vladis@gmail.com>
2025-11-25 18:55:53 +0300

    iter7: comment update view with params.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -25,7 +25,7 @@ func main() {
 	r.GET("/", handler.Root(s))
 
 	r.POST("/update", handler.Update(s))
-	r.POST("/update/:metricType/:metricName/:metricValue", handler.UpdateTypeNameValue(s))
+	// r.POST("/update/:metricType/:metricName/:metricValue", handler.UpdateTypeNameValue(s))
 
 	r.POST("/value", handler.Value(s))
 	r.GET("/value/:metricType/:metricName", handler.ValueTypeName(s))
