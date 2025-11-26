30d9cec57aee3719fcf618864d3b0c21b0bd26ca
Vladislav <ragozin.vladis@gmail.com>
2025-11-26 19:52:45 +0300

    iter7: fixed value to lower.

--- internal/handler/value.go
+++ internal/handler/value.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"strings"
 
 	models "github.com/Vladis-r/metrics.git/internal/model"
 	"github.com/gin-gonic/gin"
@@ -10,12 +11,13 @@ import (
 // Value - POST handler for get metric with JSON in body.
 func Value(s *models.MemStorage) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var item models.Metric
-		if err := c.ShouldBindJSON(&item); err != nil {
+		var metric models.Metric
+		if err := c.ShouldBindJSON(&metric); err != nil {
 			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		existItem, ok := s.Store[item.ID]
+		id := strings.ToLower(metric.ID)
+		existItem, ok := s.Store[id]
 		if !ok {
 			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "Metric not found"})
 			return
