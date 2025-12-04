87e51474cf97233011e4c024f3dade5682ce643c
Vladislav <ragozin.vladis@gmail.com>
2025-12-04 20:20:40 +0300

    item7: up saveIntMetric.

--- internal/model/metrics.go
+++ internal/model/metrics.go
@@ -84,6 +84,9 @@ func (m *MemStorage) saveFloatMetric(id string, metricValue float64) {
 }
 
 func (m *MemStorage) saveIntMetric(id string, metricValue int64) {
+	if item, ok := m.Store[id]; ok {
+		metricValue += *item.Delta
+	}
 	m.Store[id] = Metric{
 		ID:    id,
 		MType: Counter,
